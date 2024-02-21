dfeada9460ab178f8fd43de420d7a3a46af31576
thomasbouman1505 <65914841+thomasbouman1505@users.noreply.github.com>
2024-02-21 12:05:28 +0100

    apdb

---
+++ LandBouwKas/data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace LandBouwKas.data
+{
+    internal class AppDbContext : DbContext
+
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=localhost;" +
+                "port=3306;" +
+                "user=root;" +
+                "password=root;" +
+                "database=LandBouwKas",
+                ServerVersion.Parse("8.0.30-mariadb")
+                );
+        }
+    }
+}
